72d023e02ae538c00fbf919191c6fe4746c0d4d4
Edison <krylov2310@gmail.com>
2024-12-12 16:23:02 +0500

    Update views.py

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
 
 
+menu_ = {'Главная': ["http://127.0.0.1:8000/"], 'Магазин': ["http://127.0.0.1:8000/games/"],
+         'Корзина': ["http://127.0.0.1:8000/cart/"]}
+
+
 # Create your views here.
 def platform(request):
-    return render(request, 'third_task/platform.html')
+    pagename = 'Главная страница'
+    menu = menu_
+    context = {
+        'pagename': pagename,
+        'menu': menu,
+    }
+    return render(request, 'third_task/platform.html', context)
 
 
 def games(request):
-    return render(request, 'third_task/games.html')
+    pagename = 'Игры'
+    content = {'games': ["Atomic Heart", "Cyberpunk 2077", "PayDay 2"],
+               'programs': ["Microsoft Office", "Windows 10 Pro"]}
+    context = {
+        'pagename': pagename,
+        'menu': menu_,
+        'content': content
+    }
+    return render(request, 'third_task/games.html', context)
 
 
 def cart(request):
-    return render(request, 'third_task/cart.html')
+    pagename = 'Корзина'
+    content = 'Извините, ваша корзина пуста'
+    context = {
+        'pagename': pagename,
+        'menu': menu_,
+        'content': content
+    }
+    return render(request, 'third_task/cart.html', context)
